5a5f33abd460fd44e7e253566adf57486ed560bf
Lyasikzzz <dyadyapavliklyas@gmail.com>
2023-02-08 14:03:37 +0300

    final project

--- src/index.js
+++ src/index.js
@@ -38,9 +38,29 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let resultTen, resultDuo, result = '', final = '';
+    for (let i = 0; i < expr.length;) {
+        resultTen = expr.slice(i, i + 10);
+        i += 10;
+        if (resultTen != "**********") {
+            for (let j = 0; j < resultTen.length;) {
+                resultDuo = resultTen.slice(j, j + 2);
+                j += 2;
+                if (resultDuo == "10") {
+                    result += ".";  
+                } else if (resultDuo == "11") {
+                    result += "-";
+                } 
+            }
+            final += MORSE_TABLE[result];
+        } else {
+            final += " ";
+        }
+        result = '';
+    }
+    return final;
 }
 
 module.exports = {
     decode
-}
+};
